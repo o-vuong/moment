d3bea1e45b5bd3068303ca7048fe277b18509d62
o-vuong <oscarvuong@gmail.com>
2020-02-17 09:42:20 -0800

    med model built

--- app/models/med.rb
+++ app/models/med.rb
@@ -1,2 +1,11 @@
 class Med < ApplicationRecord
+    belongs_to :user, optional: true
+
+    validates_presence_of :name, :dose, :prescribed, :first_dose, :notes
+    validates :dose, numericality: { only_integer: true }
+  
+    def make_title_case
+      self.name = self.name.titlecase
+      self.prescribed = self.prescribed.titlecase
+    end
 end
