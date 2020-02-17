3b774515abd2d1a1e87b0807d5421d674dc000b5
o-vuong <oscarvuong@gmail.com>
2020-02-17 12:10:41 -0800

    components made

---
+++ client/src/components/users/Details.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+//
+// class Details extends Component {
+//   constructor(props){
+//     super(props)
+//   }
+//
+//   render(){
+//     return (
+//       <div className="UserDetails">
+//         {this.props.users.map(user =>
+//           <h1>{user.name}</h1>
+//         )}
+//       </div>
+//     )
+//   }
+// };
+//
+// export default Details;
