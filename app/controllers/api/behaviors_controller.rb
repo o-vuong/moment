291efd7e32a55c41a93a69db97b293ff20f84018
o-vuong <oscarvuong@gmail.com>
2020-02-17 09:40:43 -0800

    behavior controller built out

--- app/controllers/api/behaviors_controller.rb
+++ app/controllers/api/behaviors_controller.rb
@@ -1,2 +1,33 @@
-class BehaviorsController < ApplicationController
-end
+class Api::BehaviorsController < ApplicationController
+
+    before_action :authenticate_user
+    before_action :set_behavior, only: [:show, :update]
+  
+    def index
+      render json: Behavior.all
+    end
+  
+    def show
+      render json: @behavior
+    end
+  
+    def update
+      if @behavior.update(behavior_params)
+        render json: @behavior
+      else
+        render json: { message: @behavior.errors }, status: 400
+      end
+    end
+  
+    private
+  
+      def set_behavior
+        @behavior = Behavior.find_by(id: params[:id])
+      end
+  
+      def behavior_params
+        params.require(:behavior).permit(:name, :details, condition_ids:[], :conditions_attributes => [:id, :name, :_destroy])
+      end
+  
+  end
+  
