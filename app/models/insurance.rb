efcad1a7b010c2aab39327bf2507f32f9dc6b77c
o-vuong <oscarvuong@gmail.com>
2020-02-17 09:42:11 -0800

    insurance model built

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,2 +1,10 @@
 class Insurance < ApplicationRecord
+    belongs_to :user, optional: true
+
+    validates_presence_of :name, :address, :phone, :notes
+  
+    def make_title_case
+      self.name = self.name.titlecase
+      self.address = self.address.titlecase
+    end
 end
